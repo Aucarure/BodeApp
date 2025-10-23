3d7ec96ee3ae0528c4abd5f16f0479fa9221542c
Aucarure <victor.aucarure@tecsup.edu.pe>
2025-10-23 18:53:21 -0500

    crear pantallas vacías (Home, Productos, Ventas, Compras, Reportes)

--- app/src/main/java/com/tecsup/bodeapp/MainActivity.kt
+++ app/src/main/java/com/tecsup/bodeapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.tecsup.bodeapp.ui.HomeScreen
 import com.tecsup.bodeapp.ui.theme.BodeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BodeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
     }
---
+++ app/src/main/java/com/tecsup/bodeapp/ui/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.tecsup.bodeapp.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text("Home")
+}
